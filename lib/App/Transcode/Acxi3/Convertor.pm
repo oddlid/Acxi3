52d896677d72986eb8cbcf3de1a96c37f8b8ffd2
Odd Eivind Ebbesen <odd@oddware.net>
2013-02-22 22:32:23 +0100

    Tried to remember my purpose with the Convertor, and think I know now...

--- lib/App/Transcode/Acxi3/Convertor.pm
+++ lib/App/Transcode/Acxi3/Convertor.pm
@@ -8,23 +8,35 @@ use mro 'c3';
 use Carp;
 use IPC::Cmd qw(can_run run run_forked QUOTE);
 
-
 sub new {
    my $class = shift;
    my %args  = @_;
-   my %cfg = (
+   my %cfg   = (
       decoder      => undef,
       decoder_args => [],
+      infile       => undef,
       encoder      => undef,
       encoder_args => [],
+      outfile      => undef,
    );
    @cfg{ keys(%args) } = values(%args);    # merge defaults and params
-   my $self = bless(\%cfg, $class);   # only create if not already alive
+   my $self = bless(\%cfg, $class);        # only create if not already alive
    return $self;
 }
 
+sub cmdline {
+   my $self = shift;
+   return sprintf(
+      "%s %s %s | %s %s %s",
+      $self->{decoder}, $self->{decoder_args}, QUOTE . $self->{infile} . QUOTE,
+      $self->{encoder}, $self->{encoder_args}, QUOTE . $self->{outfile} . QUOTE
+   );
+}
 
-
+sub run {
+   my $self = shift;
+   return run_forked($self->cmdline, @_);
+}
 
 1;
 __END__
--- t/03-Convertor.t
+++ t/03-Convertor.t
@@ -10,8 +10,18 @@ BEGIN {
 }
 require_ok('App::Transcode::Acxi3::Convertor');
 
-my $cvt = App::Transcode::Acxi3::Convertor->new();
+my $cvt = App::Transcode::Acxi3::Convertor->new(
+   decoder      => '/usr/bin/flac',
+   decoder_args => '-s -d -c',
+   infile       => '/home/oddee/gris.flac',
+   encoder      => '/usr/bin/lame',
+   encoder_args => '-h -V 6 --ta "Madder Mortem" --tl "Desiderata" --tt "The Flood to Come" - -o',
+   outfile      => '/home/oddee/pikk.mp3',
+);
 ok(defined($cvt), ' new() returned something');
 ok($cvt->isa('App::Transcode::Acxi3::Convertor'), ' ...and it is of the right class');
+
+#print(Dumper($cvt->cmdline));
+printf("Command: %s\n", $cvt->cmdline);
 
 done_testing();
